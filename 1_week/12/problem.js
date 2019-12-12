4a163cd1ee63fbb4944d1e483de2c46a54c60427
dongqui <kimwinner90@gmail.com>
2019-12-12 23:45:22 +0900

    sumAll test

---
+++ 1_week/12/problem.js
@@ -0,0 +1,18 @@
+/*
+
+두 자연수 a, b가 주어졌을 때 a ~ b 사이의 모든 수를 더해주세요
+(a <= b)
+
+ex)
+input 1, 10   output 55
+input 1, 3   output  6
+input 5 5   output 5
+
+ */
+
+function sumAll(a, b) {
+
+}
+
+module.exports = sumAll;
+
